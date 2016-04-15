e79aef469d4ef4e34251051448f55c137231ed20
Vitaly Andrianov <vitalya@ti.com>
2016-04-15 08:26:32 -0400

    arm: move aes_glue.h to the arch/arm/include/asm directory
    The aes_glue.h defines function which might be used from drivers.
    This commit moves the file to the arch/arm/include/asm to be accessible
    publicly.
    
    Signed-off-by: Vitaly Andrianov <vitalya@ti.com>
    Signed-off-by: Jacob Stiffler <j-stiffler@ti.com>

--- arch/arm/crypto/aesbs-glue.c
+++ arch/arm/crypto/aesbs-glue.c
@@ -13,8 +13,7 @@
 #include <crypto/ablk_helper.h>
 #include <crypto/algapi.h>
 #include <linux/module.h>
-
-#include "aes_glue.h"
+#include <asm/aes_glue.h>
 
 #define BIT_SLICED_KEY_MAXSIZE	(128 * (AES_MAXNR - 1) + 2 * AES_BLOCK_SIZE)
 
